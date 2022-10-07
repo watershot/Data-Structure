42cb122ed4380ce5b9743e38e54f2855985fb7b4
박경원 <57476769+watershot@users.noreply.github.com>
2022-10-07 11:03:25 +0900

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,22 +1,36 @@
-﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿
 
-#include <iostream>
+# include <iostream>
+# include <stdio.h>
+# define MAX_SIZE 5
 
-int main()
-{
-    
-    std::cout << "Hello World!\n"; //printf("Hello World!);
-    
+// 버블 정렬
+void bubble_sort(int list[], int n) {
+    int i, j, temp;
+
+    for (i = n - 1; i > 0; i--) {
+        // 0 ~ (i-1)까지 반복
+        for (j = 0; j < i; j++) {
+            // j번째와 j+1번째의 요소가 크기 순이 아니면 교환
+            if (list[j] < list[j + 1]) {
+                temp = list[j];
+                list[j] = list[j + 1];
+                list[j + 1] = temp;
+            }
+        }
+    }
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+void main() {
+    int i;
+    int n = MAX_SIZE;
+    int list[n] = { 7, 4, 5, 1, 3 };
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+    // 버블 정렬 수행
+    bubble_sort(list, n);
+
+    // 정렬 결과 출력
+    for (i = 0; i < n; i++) {
+        printf("%d\n", list[i]);
+    }
+}
